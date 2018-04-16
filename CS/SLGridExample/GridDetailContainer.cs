8a4fb366fcdb9c4e42baadc3a4159e155d0d60cb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/SLGridExample/GridDetailContainer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+using System.Windows.Data;
+
+namespace SLGridExample {
+    public class GridDetailContainer : ContentPresenter {
+
+        public static readonly DependencyProperty IsVisibleProperty =
+            DependencyProperty.Register("IsVisible", typeof(bool), typeof(GridDetailContainer), new PropertyMetadata(false, new PropertyChangedCallback(GridPropertyChangedCallback)));
+
+        public bool IsVisible {
+            get { return (bool)GetValue(IsVisibleProperty); }
+            set { SetValue(IsVisibleProperty, value); }
+        }
+
+        public static readonly DependencyProperty GridProperty =
+            DependencyProperty.Register("Grid", typeof(GridControl), typeof(GridDetailContainer), new PropertyMetadata(null, new PropertyChangedCallback(GridPropertyChangedCallback)));
+
+        public GridControl Grid {
+            get { return (GridControl)GetValue(GridProperty); }
+            set { SetValue(GridProperty, value); }
+        }
+
+        static void GridPropertyChangedCallback(DependencyObject s, DependencyPropertyChangedEventArgs e) {
+            ((GridDetailContainer)s).OnChanged();
+        }
+
+        Binding gridItemsSourceBinding;
+        public Binding GridItemsSourceBinding {
+            get { return gridItemsSourceBinding; }
+            set {
+                gridItemsSourceBinding = value;
+                OnChanged();
+            }
+        }
+
+        void OnChanged() {
+            Content = Grid;
+            if (IsVisible) {
+                Visibility = Visibility.Visible;
+                if (Grid != null && GridItemsSourceBinding != null)
+                    Grid.SetBinding(GridControl.DataSourceProperty, GridItemsSourceBinding);
+            } else {
+                Visibility = Visibility.Collapsed;
+                if (Grid != null)
+                    Grid.ClearValue(GridControl.DataSourceProperty);
+            }
+        }
+
+    }
+}
---
+++ CS/SLGridExample/TestDataClasses.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace SLGridExample {
+
+    public class TestRecordList : List<TestRecord> {
+    }
+
+    public class TestRecord : INotifyPropertyChanged {
+
+        public string Description { get; set; }
+
+        public string MasterRecordID { get; set; }
+
+        DetailsList details;
+        public DetailsList DetailedInfo {
+            get {
+                return details;
+            }
+            set {
+                if (details == value)
+                    return;
+                details = value;
+                RaisePropertyChaged("DetailedInfo");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void RaisePropertyChaged(string propertyName) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+
+    public class DetailsList : List<Details> {
+        public override string ToString() {
+            return "Detail (Count = " + Count.ToString() + ")";
+        }
+    }
+
+    public class Details {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
